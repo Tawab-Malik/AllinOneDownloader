965d709055ae92ac34bc1162cb7ef29960afb345
Tawab Malikx <105295180+Tawab-Malik@users.noreply.github.com>
2025-04-03 11:53:45 +0500

    puhed

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["source.unsplash.com"],
-  }
+  },
+  experimental: {
+    optimizeCss: false, // Disables LightningCSS optimization
+  },
 };
 
 export default nextConfig;
