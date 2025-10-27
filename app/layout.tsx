761e843338f5b7b47adf86d49e0dc9e9b412796d
Tawab-Malik <tchannar5@gmail.com>
2025-10-27 21:12:29 +0500

    worked on Api

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "AllinOne Downloader",
-  description: "This is Free App for Downoading reels and videos",
+  description: "This is Free App for Downoading Instagram reels and videos",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,7 @@ import NewDownloader from "./components/ReelsDownloader";
 export default function Home() {
   return (
     <div className=" ">
-      
          <NewDownloader/>
-         
-         
     </div>
   );
 }
